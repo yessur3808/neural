c96caeff74bc52278396ca21bf30df1c2c719a7b
yu120 <18730214@qq.com>
2016-07-29 23:08:01 +0800

    Signed-off-by: yu120 <18730214@qq.com>

---
+++ src/test/java/cn/ms/neural/moduler/degrade/FaultTolerantDegradeTest.java
@@ -0,0 +1,115 @@
+package cn.ms.neural.moduler.degrade;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import cn.ms.neural.common.URL;
+import cn.ms.neural.common.exception.ProcessorException;
+import cn.ms.neural.common.exception.degrade.DegradeException;
+import cn.ms.neural.common.spi.ExtensionLoader;
+import cn.ms.neural.moduler.Moduler;
+import cn.ms.neural.moduler.extension.degrade.IDegrade;
+import cn.ms.neural.moduler.extension.degrade.processor.IDegradeProcessor;
+import cn.ms.neural.moduler.extension.degrade.type.DegradeType;
+import cn.ms.neural.moduler.extension.degrade.type.StrategyType;
+
+/**
+ * 容错降级测试
+ * 
+ * @author lry
+ */
+public class FaultTolerantDegradeTest {
+
+	@SuppressWarnings("unchecked")
+	IDegrade<String, String> degrade=ExtensionLoader.getExtensionLoader(IDegrade.class).getAdaptiveExtension();
+	
+	@Test
+	public void degrade_FAULTTOLERANT_NULL() throws Throwable {
+		try {
+			Moduler<String, String> moduler=new Moduler<>();
+			moduler.setUrl(URL.valueOf("http://127.0.0.1:8080/degrade/?"
+					+ "degrade.switch=true&"
+					+ "degrade.degradetype="+DegradeType.FAULTTOLERANT
+					+ "&degrade.strategytype="+StrategyType.NULL));
+			
+			degrade.setModuler(moduler);
+			
+			String res=degrade.degrade("请求报文", new IDegradeProcessor<String, String>() {
+				@Override
+				public String processor(String req, Object... args) throws ProcessorException {
+					throw new RuntimeException("模拟容错失败");
+				}
+				@Override
+				public String mock(String req, Object... args) throws DegradeException {
+					return "这是MOCK响应报文";
+				}
+			}, null);
+
+			Assert.assertEquals(null, res);
+		} catch (Throwable t) {
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void degrade_FAULTTOLERANT_EXCEPTION() throws Throwable {
+		try {
+			Moduler<String, String> moduler=new Moduler<>();
+			moduler.setUrl(URL.valueOf("http://127.0.0.1:8080/degrade/?"
+					+ "degrade.switch=true&"
+					+ "degrade.degradetype="+DegradeType.FAULTTOLERANT
+					+ "&degrade.strategytype="+StrategyType.EXCEPTION));
+			
+			degrade.setModuler(moduler);
+			
+			degrade.degrade("请求报文", new IDegradeProcessor<String, String>() {
+				@Override
+				public String processor(String req, Object... args) throws ProcessorException {
+					throw new RuntimeException("模拟容错失败");
+				}
+				@Override
+				public String mock(String req, Object... args) throws DegradeException {
+					return "这是MOCK响应报文";
+				}
+			}, null);
+			
+			Assert.assertTrue(false);
+		} catch (Throwable t) {
+			t.printStackTrace();
+			if(t instanceof DegradeException){
+				Assert.assertTrue(true);
+			}else{
+				Assert.assertTrue(false);
+			}
+		}
+	}
+	
+	@Test
+	public void degrade_FAULTTOLERANT_MOCK() throws Throwable {
+		try {
+			Moduler<String, String> moduler=new Moduler<>();
+			moduler.setUrl(URL.valueOf("http://127.0.0.1:8080/degrade/?"
+					+ "degrade.switch=true&"
+					+ "degrade.degradetype="+DegradeType.FAULTTOLERANT
+					+ "&degrade.strategytype="+StrategyType.MOCK));
+			
+			degrade.setModuler(moduler);
+			
+			String res=degrade.degrade("请求报文", new IDegradeProcessor<String, String>() {
+				@Override
+				public String processor(String req, Object... args) throws ProcessorException {
+					throw new RuntimeException("模拟容错失败");
+				}
+				@Override
+				public String mock(String req, Object... args) throws DegradeException {
+					return "这是MOCK响应报文";
+				}
+			}, null);
+			
+			Assert.assertEquals("这是MOCK响应报文", res);
+		} catch (Throwable t) {
+			Assert.assertTrue(false);
+		}
+	}
+	
+}
