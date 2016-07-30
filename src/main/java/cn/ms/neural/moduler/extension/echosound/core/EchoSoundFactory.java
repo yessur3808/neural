011d601ca28d97d8f5c2e97db97672542ba0dae5
yu120 <18730214@qq.com>
2016-07-30 18:01:22 +0800

    1

---
+++ src/main/java/cn/ms/neural/moduler/extension/echosound/core/EchoSoundFactory.java
@@ -0,0 +1,63 @@
+package cn.ms.neural.moduler.extension.echosound.core;
+
+import cn.ms.neural.common.exception.EchoSoundException;
+import cn.ms.neural.moduler.Conf;
+//github.com/yu120/neural
+import cn.ms.neural.moduler.Moduler;
+import cn.ms.neural.moduler.extension.echosound.IEcho;
+import cn.ms.neural.moduler.extension.echosound.IEchoSound;
+import cn.ms.neural.moduler.extension.echosound.conf.EchoSoundConf;
+import cn.ms.neural.moduler.extension.echosound.processor.IEchoSoundProcessor;
+import cn.ms.neural.moduler.extension.echosound.type.EchoSoundType;
+
+/**
+ * 回声探测
+ * 
+ * @author lry
+ *
+ * @param <REQ>
+ * @param <RES>
+ */
+public class EchoSoundFactory<REQ, RES> implements IEchoSound<REQ, RES> {
+
+	Moduler<REQ, RES> moduler;
+	/**
+	 * 开关
+	 */
+	boolean echoSoundSwitch=true;
+
+	@Override
+	public void notify(Moduler<REQ, RES> moduler) {
+		this.moduler=moduler;
+
+		echoSoundSwitch=this.moduler.getUrl().getModulerParameter(Conf.ECHOSOUND, EchoSoundConf.SWITCH_KEY, EchoSoundConf.SWITCH_DEF_VAL);
+	}
+
+	@Override
+	public void init() throws Throwable {
+		
+	}
+
+	@Override
+	public RES echosound(EchoSoundType echoSoundType, REQ req, IEchoSoundProcessor<REQ, RES> processor, IEcho<REQ, RES> echo, Object... args) throws EchoSoundException {
+		if(!echoSoundSwitch){//开关校验
+			return processor.echosound(req, processor, args);
+		}
+		
+		switch (echoSoundType) {
+		case REQ:
+			REQ echoREQ=echo.$echo(args);//模拟请求
+			return processor.echosound(echoREQ, processor, args);
+		case RES:
+			return echo.$rebound(args);//模拟响应
+		default:
+			return processor.echosound(req, processor, args);//费回声探测
+		}
+	}
+	
+	@Override
+	public void destory() throws Throwable {
+		
+	}
+
+}
