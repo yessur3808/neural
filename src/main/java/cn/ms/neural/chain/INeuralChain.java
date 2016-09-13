39e9e0d166fea7587dc43c475e3a52984cdd748a
yu120 <595208882@qq.com>
2016-09-13 08:59:37 +0800

    调用链结构变更

--- src/main/java/cn/ms/neural/chain/INeuralChain.java
+++ src/main/java/cn/ms/neural/chain/INeuralChain.java
@@ -5,15 +5,35 @@ import java.util.Map;
 import cn.ms.neural.moduler.extension.echosound.type.EchoSoundType;
 import cn.ms.neural.processor.INeuralProcessor;
 
+/**
+ * 微服务神经元调用链
+ * 
+ * @author lry
+ *
+ * @param <REQ>
+ * @param <RES>
+ */
 public interface INeuralChain<REQ, RES> {
 
+	/**
+	 * 设置调用链
+	 * 
+	 * @param neuralChain
+	 */
 	void setNeuralChain(INeuralChain<REQ, RES> neuralChain);
-	
-	RES chain(REQ req, 
-			String neuralId, 
-			EchoSoundType echoSoundType, 
-			Map<String, Object> blackWhiteIdKeyVals,
-			INeuralProcessor<REQ, RES> processor, 
-			Object... args);
+
+	/**
+	 * 调用链
+	 * 
+	 * @param req
+	 * @param neuralId
+	 * @param echoSoundType
+	 * @param blackWhiteIdKeyVals
+	 * @param processor
+	 * @param args
+	 * @return
+	 */
+	RES chain(REQ req, String neuralId, EchoSoundType echoSoundType, Map<String, Object> blackWhiteIdKeyVals,
+			INeuralProcessor<REQ, RES> processor, Object... args);
 
 }
--- src/main/java/cn/ms/neural/chain/support/AbstractNeuralChain.java
+++ src/main/java/cn/ms/neural/chain/support/AbstractNeuralChain.java
@@ -3,11 +3,18 @@ package cn.ms.neural.chain.support;
 import cn.ms.neural.chain.INeuralChain;
 import cn.ms.neural.moduler.Moduler;
 
+/**
+ * 微服务神经元抽象类调用链
+ * 
+ * @author lry
+ *
+ * @param <REQ>
+ * @param <RES>
+ */
 public abstract class AbstractNeuralChain<REQ, RES> implements INeuralChain<REQ, RES> {  
     
-	protected Moduler<REQ, RES> moduler;
-	protected INeuralChain<REQ, RES> neuralChain;  
-    
+	public Moduler<REQ, RES> moduler;
+	public INeuralChain<REQ, RES> neuralChain;  
     
 	public AbstractNeuralChain(Moduler<REQ, RES> moduler) {
 		this.moduler = moduler;
